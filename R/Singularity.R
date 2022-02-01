6bae91a7cbfa897ce0f2e0357a83cbf6e48a29b6
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2022-02-01 09:57:56 +0100

    Add official R recipe

---
+++ R/Singularity.R
@@ -0,0 +1,31 @@
+BootStrap: debootstrap
+OSVersion: xenial
+MirrorURL: http://archive.ubuntu.com/ubuntu/
+
+%post
+    sed -i 's/main/main restricted universe/g' /etc/apt/sources.list
+    apt update
+  
+    # Install R, Python, misc. utilities
+    apt install -y libopenblas-dev r-base-core libcurl4-openssl-dev libopenmpi-dev openmpi-bin openmpi-common openmpi-doc openssh-client openssh-server libssh-dev wget vim git nano git cmake  gfortran g++ curl wget python autoconf bzip2 libtool libtool-bin python-pip python-dev
+    apt clean
+    locale-gen en_US.UTF-8
+  
+    # Install Tensorflow
+    pip install tensorflow
+  
+    # Install required R packages
+    R --slave -e 'install.packages("devtools", repos="https://cloud.r-project.org/")'
+    R --slave -e 'devtools::install_github("rstudio/tensorflow")'
+
+%test
+  #!/bin/sh
+  exec R --slave -e "library(tensorflow); \
+                     sess  <- tensorflow::tf\$Session(); \
+                     hello <- tensorflow::tf\$constant('Hello, TensorFlow!'); \
+                     sess\$run(hello)"
+
+
+%runscript
+  #!/bin/bash
+  Rscript --slave "main.R"
